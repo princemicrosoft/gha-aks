cb5b63b32ec5d32957f184f777c8b150956bd06b
Price Ninawe <prince.ninawe@PIDPIA.onmicrosoft.com>
2025-06-25 19:00:35 +0530

    Initializing repo with terraform code

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -32,10 +32,10 @@ provider "azurerm" {
     }
   }
 
-#   client_id       = var.client_id
-#   client_secret   = var.client_secret
-#   tenant_id       = var.tenant_id
-#   subscription_id = var.subscription_id
+  #   client_id       = var.client_id
+  #   client_secret   = var.client_secret
+  #   tenant_id       = var.tenant_id
+  #   subscription_id = var.subscription_id
 
   #   storage_use_azuread = true
   #   use_oidc            = true
